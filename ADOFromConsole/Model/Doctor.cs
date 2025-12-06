84b50350a3a537b60b720000d6818f5689fbe154
markusemile6238 <markusemile6238@gmail.com>
2025-12-06 15:08:01 +0100

    push ADO basic connection from console

---
+++ ADOFromConsole/Model/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOFromConsole.Model
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Ref { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Service { get; set; }
+
+    }
+}
---
+++ ADOFromConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ADOFromConsole.Model;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        // connection  basic
+        string connectionString = "Server=151.80.59.7,1433;Initial Catalog=exoDb;Persist Security Info=True;User ID=markusemile;Password=29Dec1972!;Encrypt=True;Trust Server Certificate=True";
+
+        List<Doctor> lesDoctors = new List<Doctor>();
+
+        // creation de la connection
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                // on ouvre la connection
+                connection.Open();
+                Console.WriteLine("connection reussit");
+
+                // execution d'une requete
+                string sql = "SELECT * FROM Doctors ";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lesDoctors.Add(new Doctor
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Ref = reader["Ref"].ToString() ?? "N/A",
+                                Firstname = reader["Firstname"].ToString() ?? "N/A",
+                                Lastname = reader["LastName"].ToString() ?? "n/A",
+                                Service = reader["Service"].ToString() ?? "N/A"
+                            });
+                        }
+                    }
+                }
+                ;
+            }catch(SqlException ex)
+            {
+                Console.WriteLine("Erreur de connection"+ex);
+            }
+        }
+        if (lesDoctors.Any())
+        {
+            foreach (var doc in lesDoctors)
+            {
+                Console.WriteLine($"{doc.Id,2} : {doc.Ref,2} : {doc.Firstname,-8} {doc.Lastname,-8}  -  {doc.Service}");
+            }
+            {
+                
+            }
+        }
+    }
+}
